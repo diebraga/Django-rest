48c1163c6542f7e1429ef9f32ee64991e7405a6b
unknown <diebraga0055@gmail.com>
2020-09-25 22:44:05 +0100

    added car_dealers app and model

--- milestone4/settings.py
+++ milestone4/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('MY_SECRET_SET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'accounts',
+    'car_dealers',
 ]
 
 MIDDLEWARE = [
